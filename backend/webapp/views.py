48c14f4b56c86a1b9f614c6c9c5232d969a595ab
haze-me <harrisonaka29@gmail.com>
2025-03-14 01:56:04 +0100

    logout and login features done

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    # chech the logged in user
+    if request.method == 'POST':
+        #Username = request.POST['username']
+        #password = request.POST['password']
+        username = request.POST.get('username', None)  # Returns None instead of raising an error
+        password = request.POST.get('password', None)
+        
+        # authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            
+            #then we have to redirect to the page
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('home')
+    else:
+            return render(request, 'home.html')
+
+
+
+# this func will be used, if we want to use any seperate page for login
+#def login_view(request):
+ #   pass
+
+
+def logout_view(request):
+   logout(request)
+   messages.success(request, 'You are logged out')
+   return redirect('home')
